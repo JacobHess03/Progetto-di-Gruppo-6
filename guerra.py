f8f52fbcc1f95c4dc917833784e522e611fb182f
JacobHess03 <giacomovisciotti4@gmail.com>
2025-04-14 16:16:16 +0200

    Create guerra.py

---
+++ guerra.py
@@ -0,0 +1,16 @@
+class UnitaMilitare:
+    def __init__(self, nome, numero_soldati):
+        self.nome = nome
+        self.numero_soldati = numero_soldati
+        
+        
+    def muovi(self):
+        print("Movimento")
+    
+    def attacca(self):
+        print("Attacco")
+    
+    def ritira(self):
+        print("Ritirata")
+            
+            
