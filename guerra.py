2b856a30c87da3fe8f4fee2e0c3c41b965e43f01
JacobHess03 <giacomovisciotti4@gmail.com>
2025-04-14 16:27:31 +0200

    update

--- guerra.py
+++ guerra.py
@@ -16,26 +16,37 @@ class UnitaMilitare:
 
 # Classi derivate
 class Fanteria(UnitaMilitare):
+    def __init__(self, nome, numero_soldati):
+        super(self, nome, numero_soldati)
     def costruisci_trincea(self):
         print(f"{self.nome} costruisce trincee per la difesa temporanea.")
 
 
 class Artiglieria(UnitaMilitare):
+    def __init__(self, nome, numero_soldati):
+        super(self, nome, numero_soldati)
     def calibra_artiglieria(self):
         print(f"{self.nome} calibra i pezzi di artiglieria per una maggiore precisione.")
 
 
 class Cavalleria(UnitaMilitare):
+    def __init__(self, nome, numero_soldati):
+        super(self, nome, numero_soldati)
     def esplora_terreno(self):
         print(f"{self.nome} esplora il terreno per raccogliere informazioni sul nemico.")
 
 
 class SupportoLogistico(UnitaMilitare):
+    def __init__(self, nome, numero_soldati):
+        super(self, nome, numero_soldati)
     def rifornisci_unita(self):
         print(f"{self.nome} si occupa del rifornimento e della manutenzione delle truppe.")
 
 
 class Ricognizione(UnitaMilitare):
+    
+    def __init__(self, nome, numero_soldati):
+        super(self, nome, numero_soldati)
     def conduci_ricognizione(self):
         print(f"{self.nome} conduce una missione di sorveglianza.")
 
